918b5072330c0d9758dc6d1c1f78260141455f1f
marizona <marie.jacquier@epitech.eu>
2022-04-26 10:34:48 +0200

    set up search bar get query

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import SearchBar from './SearchBar'
 
-export default function App() {
-  return (
-    <div className='ui container' style={{marginTop:'10px'}}>
-      <SearchBar/>
-    </div>
-  )
+import SearchBar from './SearchBar'
+import React, { Component } from 'react'
+
+export default class App extends Component {
+onSearchSubmit(term){
+  console.log(term)
 }
+
+  render() {
+    return (
+      <div className='ui container' style={{marginTop:'10px'}}>
+      <SearchBar onSubmit={this.onSearchSubmit}/>
+    </div>
+    )
+  }
+}
+
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = {
+    term: "",
+  };
+onFormSubmit = event =>{
+    event.preventDefault()//do not reload on submit
+    this.props.onSubmit(this.state.term)
+}
+
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-              <label>Image Search</label>
-            <input type="text" name="" id="" />
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) =>
+                this.setState({ term: e.target.value })
+              }
+            />
           </div>
         </form>
       </div>
